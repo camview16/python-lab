671df22ecff71692efd61bc885c34fadbf0486a0
Amey Rivankar <ameyar247@gmail.com>
2018-07-19 13:08:37 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 f = open('index.html','w')
 
-message  """<html>
+message = """<html>
 <head></head>
 <body><p>Hello World!</p>
 <p>New Text</p>
 <p>@5 wala commit</p>
 <p>@5.10 wala commit</p>
 <p>@5.20 wala commit</p>
-<p>@5.25 wala commit</p>
+<p>@5.25 wala commit
 </body>
 </html>"""
 
